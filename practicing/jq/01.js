ac67cbc83bac448bf3090d6201a61ebe64b38b14
Joseph Hung <arixxle@gmail.com>
2020-04-17 23:04:17 +0800

    add homework

---
+++ practicing/jq/01.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+  $('.btn-add').on('click', function(){
+    event.stopPropagation();
+    event.preventDefault();
+    var path = $("#input_img").val();
+    var n = Math.floor(Math.random()*50) + 1
+    if (path.includes('?random=')) {
+      $('#pic-box').append(`<img src="${path}${n}" alt="" class="w-50 img-o p-2">`).fadeIn()
+    } else {
+      $('#pic-box').append(`<img src="${path}" alt="" class="w-50 img-o p-2">`).fadeIn()
+    }
+    // $('#pic-box').append(`<img src="${path}${n}" alt="" class="w-50 img-o p-2">`).fadeIn()
+
+    // $('#pic-box').on('mouseover','img',function(){
+    //   $(this).fadeIn()      
+    // })
+
+    // $('img').on('mouseover',function(){
+    //   $(this).fadeOut()      
+    // })
+  })
+  $('.btn-rm').on('click', function(){
+    event.stopPropagation();
+    event.preventDefault();
+    $('img').remove()
+  })
+})
